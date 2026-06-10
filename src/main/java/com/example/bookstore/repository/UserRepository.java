7c19e79f822a1985999415f40a549094e88049b5
Jakub Kopta <jakubkopta@gmail.com>
2026-06-10 23:34:13 +0200

    Add spring data jpa repositories

---
+++ src/main/java/com/example/bookstore/repository/UserRepository.java
@@ -0,0 +1,14 @@
+package com.example.bookstore.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.bookstore.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+	Optional<User> findByEmail(String email);
+
+	boolean existsByEmail(String email);
+}
